8c430c4a758e47433d459ee304fa7c7ef20ed55b
Kaba <kaba.temas@googlemail.com>
2017-10-21 21:04:15 +0200

    Text and Scripts added
    2 Texts, which change containings by crossing a timelimit

---
+++ Example Game Project/Assets/script123Test.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script123Test : MonoBehaviour {
+
+    [SerializeField]
+    public Text TextField = null;
+    public Text NumberField = null;
+
+
+    public float standardPassingByTime;
+
+
+	// Use this for initialization
+	void Start () {
+
+     
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        standardPassingByTime = standardPassingByTime - Time.deltaTime;
+
+
+        if(standardPassingByTime < 5)
+        {
+            TextField.text = "This is a Sombrero or a Pill on a Plate!";
+            if (standardPassingByTime < 0)
+            {
+                standardPassingByTime = 10;
+            }
+           
+        }
+        if(standardPassingByTime > 5)
+        {
+            TextField.text = "RamboMambo";
+
+
+        }
+
+
+        NumberField.text = standardPassingByTime.ToString();
+
+
+
+
+
+    }
+}
